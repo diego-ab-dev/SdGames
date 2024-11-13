478a700ae32fd9615a525b685700eee16920193e
diego-ab-dev <diego12barra@gmail.com>
2024-11-12 22:48:35 -0300

    Carrousel a medias xdxdxd

--- appPrincipal/views.py
+++ appPrincipal/views.py
@@ -21,5 +21,12 @@ def producto_detalle(request, producto_id):
     producto = get_object_or_404(Producto, id=producto_id)
     return render(request, 'producto_detalle.html', {'producto': producto})
 
+def vista_carrusel(request):
+    productos = Producto.objects.all()
+    cod6 = Producto.objects.filter(nombre="Call of Duty: Black Ops 6").first() 
+    fc25 = Producto.objects.filter(nombre="Fc 25").first()
+    silent = Producto.objects.filter(nombre="Silent Hill 2").first()    
+    return render(request, 'productosmenu.html', {'productos': productos, 'cod6': cod6, 'fc25': fc25, 'silent':silent})
+
 def carrito(request):
     return render(request, 'carrito.html')
